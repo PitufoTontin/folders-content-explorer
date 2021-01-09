88209276f7a20d4fa8db462bd3da5ee256f439d8
PitufoTontin <pablotpareja@hotmail.com>
2021-01-09 17:37:58 +0100

    Created project and added gititnore file

---
+++ FoldersContentExplorer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FoldersContentExplorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
